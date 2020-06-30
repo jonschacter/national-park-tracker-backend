9aca98dca3829e56a685983d75f988ae53ac74b4
github username <github email address>
2020-06-30 13:02:44 -0700

    add signup route for users#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     get "/current_user", to: "sessions#show"
+    post "/signup", to: "users#create"
 end
