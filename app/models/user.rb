20512de39d33ca9f2bc3c9a8e57cf2af78c324ae
github username <github email address>
2020-06-30 13:02:33 -0700

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :password, presence: true
-
     has_secure_password
+    
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
