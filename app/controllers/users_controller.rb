7fb2c2d6942e5cf5c6935e26a1c7b7e2772c14f6
github username <github email address>
2020-06-30 13:02:21 -0700

    add users#create logic for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user.as_json(:except => [:password_digest, :created_at, :updated_at])
+        else
+            render :json => {
+                error: user.errors.full_messages.to_sentence
+            }
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
