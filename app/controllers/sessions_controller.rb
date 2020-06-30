da95cd3c2dd6140399176da35417eaec1c6f2c33
github username <github email address>
2020-06-29 21:21:30 -0700

    add sessions controller with create route for user log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:session][:username])
+
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: user.as_json(:except => [:password_digest, :created_at, :updated_at])
+        else
+            render :json => {
+                error: "Incorrect Username or Password"
+            }
+        end
+    end
+end
