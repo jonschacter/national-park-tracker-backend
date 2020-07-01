f7826007ec5be7d5128f6bb32d229c4e276f919d
github username <github email address>
2020-07-01 10:58:06 -0700

    add visits#index controller action

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,2 +1,12 @@
 class VisitsController < ApplicationController
+    def index
+        if logged_in?
+            visits = current_user.visits
+            render :json => visits.as_json(:except => [:created_at, :updated_at])
+        else
+            render :json => {
+                errors: "You need to log in to view this"
+            }
+        end
+    end
 end
