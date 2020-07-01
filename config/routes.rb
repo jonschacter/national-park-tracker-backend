65cd986d503679a88eb04998c5ce0bf5ed09f8dc
github username <github email address>
2020-07-01 10:22:35 -0700

    add visits resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     get "/current_user", to: "sessions#show"
     post "/signup", to: "users#create"
+    resources :visits
 end
