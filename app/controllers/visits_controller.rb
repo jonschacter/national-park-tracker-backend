6b393ab3faf2a6b04dcfb1b8ed280dbb3b451fba
github username <github email address>
2020-07-01 16:46:28 -0700

    add visits#destroy controller action

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -22,6 +22,26 @@ class VisitsController < ApplicationController
         end
     end
 
+    def destroy
+        visit = Visit.find_by(id: params[:id])
+        if visit
+            visit.destroy
+            if visit.user == current_user
+                render :json => {
+                    notice: "Visit successfully deleted"
+                }
+            else
+                render :json => {
+                    error: "You are not authorized to delete this visit"
+                }
+            end
+        else
+            render :json => {
+                error: "Visit could not be found"
+            }
+        end
+    end
+
     private
 
     def visit_params
