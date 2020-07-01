465cf43b79617bfcd614ccb0b3c2900fd3af46a9
github username <github email address>
2020-07-01 14:54:42 -0700

    add visits#create controller action

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -5,8 +5,26 @@ class VisitsController < ApplicationController
             render :json => visits.as_json(:except => [:created_at, :updated_at])
         else
             render :json => {
-                errors: "You need to log in to view this"
+                error: "You need to log in to view this"
             }
         end
+    end
+
+    def create
+        visit = Visit.new(visit_params)
+
+        if visit.save
+            render :json => visit.as_json(:except => [:created_at, :updated_at])
+        else
+            render :json => {
+                error: visit.errors.full_messages.to_sentence
+            }
+        end
+    end
+
+    private
+
+    def visit_params
+        params.require(:visit).permit(:user_id, :start_date, :end_date, :park_code)
     end
 end
