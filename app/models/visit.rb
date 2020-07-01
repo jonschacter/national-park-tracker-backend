aca9956c15c99eb813e277982df996aded8e6d86
github username <github email address>
2020-07-01 16:47:22 -0700

    add start and end date validations

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,6 @@ class Visit < ApplicationRecord
     belongs_to :user
 
     validates :park_code, presence: true, allow_blank: false
+    validates :start_date, presence: true
+    validates :end_date, presence: true
 end
