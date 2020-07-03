07604c2a5b3a459b701484892c2bdb9c4cd1d9a6
github username <github email address>
2020-07-03 14:19:18 -0700

    add review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
     belongs_to :visit
     has_one :user, through: :visit
     has_one :park, through: :visit
+    
+    validates :visit_id, presence: true
+    validates :content, presence: true
 end
