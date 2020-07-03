683490a08b56fe27f72de9eca6b409c22b9b4f59
github username <github email address>
2020-07-02 18:39:06 -0700

    update seed data with reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,6 @@
 # startArray.each do |start|
 #     Park.scrape(start)
 # end
+
+# Review.create(rating: 4, content: "Beautiful views!", visit_id: 1)
+# Review.create(rating: 1, content: "Couldn't find parking and park rangers were rude.", visit_id: 2)
