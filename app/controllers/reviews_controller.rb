cb80bdbdb9b2825741abc8d05568d40656511785
github username <github email address>
2020-07-03 14:18:27 -0700

    add create action for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,4 +25,25 @@ class ReviewsController < ApplicationController
             }
         end
     end
+
+    def create
+        review = Review.new(review_params)
+        if review.save
+            render json: review.as_json(:include => {
+                :user => {
+                    :only => [:id, :username]
+                }
+            }, except: [:created_at])
+        else
+            render :json => {
+                error: review.errors.full_messages.to_sentence
+            }
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:visit_id, :content)
+    end
 end
