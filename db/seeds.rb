bfdf58fb9858606e493a7c0c1306a3d16dd9bff0
github username <github email address>
2020-07-06 13:00:05 -0700

    seed programming to scrape NPS API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,5 @@
+startArray = [0, 49, 99, 149, 199, 249, 299, 349, 399, 449]
 
-# jon = User.create(username: "jschacter", password: "password")
-
-# jon = User.first
-# jon.visits.create(start_date: "2020-05-30", end_date: "2020-05-31", park_id: 6)
-# jon.visits.create(start_date: "2020-06-15", end_date: "2020-06-20", park_id: 49)
-
-# startArray = [0, 49, 99, 149, 199, 249, 299, 349, 399, 449]
-
-# startArray.each do |start|
-#     Park.scrape(start)
-# end
-
-# Review.create(content: "Beautiful views!", visit_id: 1)
-# Review.create(content: "Couldn't find parking and park rangers were rude.", visit_id: 2)
+startArray.each do |start|
+    Park.scrape(start)
+end
