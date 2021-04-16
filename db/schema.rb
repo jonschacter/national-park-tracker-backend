282f2d286537f3e5a32f2ebebdbb1ac15517aa5d
github username <schacter.jon@gmail.com>
2021-04-16 13:15:49 -0700

    bundle install and recreate db with postgresql for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_29_195357) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "addresses", force: :cascade do |t|
     t.string "line1"
     t.string "line2"
