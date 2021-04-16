7b4350ad3626f5ec32806b87a1daa419c9624c2a
github username <schacter.jon@gmail.com>
2021-04-16 13:15:16 -0700

    adjust migration syntax to comply with postgresql

--- db/migrate/20200702193436_change_park_id_to_integer_in_visits.rb
+++ db/migrate/20200702193436_change_park_id_to_integer_in_visits.rb
@@ -1,5 +1,6 @@
 class ChangeParkIdToIntegerInVisits < ActiveRecord::Migration[6.0]
   def change
-    change_column :visits, :park_id, :integer
+    # change_column :visits, :park_id, :integer
+    change_column :visits, :park_id, 'integer USING CAST(park_id AS integer)'
   end
 end
