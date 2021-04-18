a3860f6a2596a8ae23980e6be882c8be171131be
github username <schacter.jon@gmail.com>
2021-04-18 15:25:12 -0700

    add visit date validation regex

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -6,4 +6,5 @@ class Visit < ApplicationRecord
     validates :park_id, presence: true, allow_blank: false
     validates :start_date, presence: true
     validates :end_date, presence: true
+    validates_format_of :start_date, :end_date, :with => /\A([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))\Z/, :message => "must be formatted YYYY-MM-DD"
 end
